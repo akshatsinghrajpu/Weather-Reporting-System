67566223785c1e0ae6d2d473419c6a8c27167493
akshatsinghrajpu <117477879+akshatsinghrajpu@users.noreply.github.com>
2023-05-27 18:49:56 +0530

    Create weatherreportingsystemusingiot.c

---
+++ weatherreportingsystemusingiot.c
@@ -0,0 +1,73 @@
+//Tech Trends Shameer
+#define BLYNK_TEMPLATE_ID "TMPL3LrJNOMQG"
+#define BLYNK_TEMPLATE_NAME "Weather Reporting System"
+#define BLYNK_AUTH_TOKEN "JH0_N29pl-YDl0h78celqzdh06DxG1hC"
+
+#define BLYNK_PRINT Serial
+#include <WiFi.h>
+//#include <ESP8266WiFi.h> 
+#include <BlynkSimpleEsp32.h>
+
+#include <DHT.h>
+
+
+char auth[] = BLYNK_AUTH_TOKEN;
+
+char ssid[] = "vivo 1901";  // type your wifi name
+char pass[] = "akshat4545";  // type your wifi password
+
+BlynkTimer timer;
+
+#define rainAnalog 35
+#define rainDigital 34
+
+#define DHTPIN 27 //Connect Out pin to D2 in NODE MCU
+#define DHTTYPE DHT11  
+DHT dht(DHTPIN, DHTTYPE);
+
+
+void sendSensor()
+{
+  float h = dht.readHumidity();
+  float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+    Blynk.virtualWrite(V0, t);
+    Blynk.virtualWrite(V1, h);
+    Serial.print("Temperature : ");
+    Serial.print(t);
+    Serial.print("    Humidity : ");
+    Serial.println(h);
+}
+void setup()
+{   
+  
+  Serial.begin(115200);
+  
+
+  Blynk.begin(auth, ssid, pass);
+  dht.begin();
+  timer.setInterval(100L, sendSensor);
+     
+  pinMode(rainDigital,INPUT);
+ 
+  }
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
+    int rainAnalogVal = analogRead(rainAnalog);
+  int rainDigitalVal = digitalRead(rainDigital);
+
+  Serial.print(rainAnalogVal);
+  Serial.print("\t");
+  Serial.println(rainDigitalVal);
+  Blynk.virtualWrite(V2,rainDigitalVal);
+  delay(200);
+ }
